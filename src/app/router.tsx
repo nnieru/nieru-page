88209c06a4f1c4a8d42bb0485c7f2f7c922b514c
Nathaniel Andrian <nathaniel.andrian@binus.edu>
2025-02-18 15:25:10 +0700

    change route intop hash router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { routes } from "./routes";
 
-export const router = createBrowserRouter(routes);
+// export const router = createBrowserRouter(routes);
+export const router = createHashRouter(routes); // for static host like Github pages
